234f50490b8a181163a3be9aeedd394c96a4073e
Alina Dryzhynskaya <dryzhynskayaalina@gmail.com>
2023-01-01 20:02:41 +0300

    Add Role Initilizer

---
+++ DataAccess/RoleInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using CoffeeBean.Entity;
+
+namespace CoffeeBean.DataAccess
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "admin@gmail.com";
+            string password = "_Aa123456";
+            if (await roleManager.FindByNameAsync("Admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+            if (await roleManager.FindByNameAsync("User") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("User"));
+            }
+            if (await roleManager.FindByNameAsync("Banned") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Banned"));
+            }
+            if (await userManager.FindByEmailAsync(adminEmail) == null)
+            {
+                AppUser admin = new AppUser { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+        }
+    }
+}
